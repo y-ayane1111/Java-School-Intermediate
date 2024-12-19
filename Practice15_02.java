cb21cd6a1f617ef42157953063d9857ed2c7d668
y-ayane1111 <a.yamamoto4121@gmail.com>
2024-12-19 21:11:19 +0900

    Add files via upload

---
+++ Practice15_02.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Practice15_02 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("kudo");
+        list.add("mitoma");
+        list.add("kamada");
+        list.add("kamada");
+
+        Set<String> set = new TreeSet<String>(list);
+        for (String s : set) {
+            System.out.println(s);
+        }
+
+    }
+
+}
