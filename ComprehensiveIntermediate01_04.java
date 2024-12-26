29263dd88af2b8babd7d50ef80ee6be82ba15f31
y-ayane1111 <a.yamamoto4121@gmail.com>
2024-12-26 20:18:10 +0900

    Add files via upload

---
+++ ComprehensiveIntermediate01_04.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ComprehensiveIntermediate01_04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("人数を入力してください >");
+        int people = sc.nextInt();
+
+        number(people);
+    }
+
+    public static void number(int people) {
+        if (1 <= people && people < 5) {
+            System.out.println("料金は" + (people * 600) + "円です");
+
+        } else if (5 <= people && people < 20) {
+            System.out.println("料金は" + (people * 500) + "円です");
+        } else {
+            System.out.println("料金は" + (people * 550) + "円です");
+        }
+    }
+}
