0964fba2ca75ede461ded7683de43343f0f0e7cc
y-ayane1111 <a.yamamoto4121@gmail.com>
2025-02-02 00:08:28 +0900

    Add files via upload

--- ComprehensiveIntermediate02_03.java
+++ ComprehensiveIntermediate02_03.java
@@ -8,7 +8,7 @@ public class ComprehensiveIntermediate02_03 {
         System.out.println("begin と入力してください＞ ");
         String word = sc.nextLine();
         if (word.equals("begin")) {// beginを入力させる
-            for (int i = 0; i < 30; i++) {// 30回繰り返す
+            for (int i = 1; i <= 30; i++) {// 30回繰り返す
                 Random rand = new Random();// 乱数を定義
 
                 int arr1 = rand.nextInt(10);// 1つ目の整数
@@ -18,8 +18,12 @@ public class ComprehensiveIntermediate02_03 {
                 System.out.println(i + "回目" + arr1 + arr2 + arr3);// 繰り返しの回数と出た数字を表示
 
                 if (arr1 == arr2 && arr2 == arr3) {// ゾロ目を定義
-                    System.out.println(i + "回目で大当たりです。おめでとうございます。");
+                    System.out.println(i + "回目" + arr1 + arr2 + arr3 + "で大当たりです。おめでとうございます。");
                     break;// 終了させる
+
+                }
+                if (i == 30 && arr1 != arr2 && arr2 != arr3) {
+                    System.out.println("残念でした。終わります。");
                 }
             }
 
